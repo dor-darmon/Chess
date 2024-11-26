bba6d98e145057287057f8d6e87461451580ede1
dor-darmon <161502302+dor-darmon@users.noreply.github.com>
2024-11-26 13:24:08 +0200

    Update CheckmateDetector.java

--- chees/src/CheckmateDetector.java
+++ chees/src/CheckmateDetector.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 /**
  * Component of the Chess game that detects check mates in the game.
  *
- * @author Jussi Lundstedt
+ * @author Dor darmon
  */
 public class CheckmateDetector {
     private Board b;
